3ee223b283523746e9da5f818ea4013438d04475
oussema makni <oussema.makni@esprit.tn>
2021-02-25 19:44:49 +0100

    fix

---
+++ src/Entity/OffreEmploi.php
@@ -0,0 +1,254 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OffreEmploiRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OffreEmploiRepository::class)
+ */
+class OffreEmploi
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $idOffre;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=user::class, inversedBy="offres")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $idRecruteur;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $poste;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_debut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_expiration;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=user::class)
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $idCandidat;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $maxSalary;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $minSalary;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $email;
+
+    public function getIdOffre(): ?int
+    {
+        return $this->idOffre;
+    }
+
+    public function setIdOffre(int $idOffre): self
+    {
+        $this->idOffre = $idOffre;
+
+        return $this;
+    }
+
+    public function getIdRecruteur(): ?User
+    {
+        return $this->idRecruteur;
+    }
+
+    public function setIdRecruteur(?User $idUser): self
+    {
+        $this->idRecruteur = $idUser;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getPoste(): ?string
+    {
+        return $this->poste;
+    }
+
+    public function setPoste(string $poste): self
+    {
+        $this->poste = $poste;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->date_debut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $date_debut): self
+    {
+        $this->date_debut = $date_debut;
+
+        return $this;
+    }
+
+    public function getDateExpiration(): ?\DateTimeInterface
+    {
+        return $this->date_expiration;
+    }
+
+    public function setDateExpiration(\DateTimeInterface $date_expiration): self
+    {
+        $this->date_expiration = $date_expiration;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getIdCandidat(): ?user
+    {
+        return $this->idCandidat;
+    }
+
+    public function setIdCandidat(?user $idCandidat): self
+    {
+        $this->idCandidat = $idCandidat;
+
+        return $this;
+    }
+
+    public function getMaxSalary(): ?int
+    {
+        return $this->maxSalary;
+    }
+
+    public function setMaxSalary(?int $maxSalary): self
+    {
+        $this->maxSalary = $maxSalary;
+
+        return $this;
+    }
+
+    public function getMinSalary(): ?int
+    {
+        return $this->minSalary;
+    }
+
+    public function setMinSalary(?int $minSalary): self
+    {
+        $this->minSalary = $minSalary;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(?string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
